93bbdb3c996fc663f00206732e3eac8e48c11489
Strahinja Ljaljevic <STRAHINJA@aleksandars-macbook-air.local>
2018-12-05 20:27:41 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Strahinja Ljaljević on 12/5/18.
+//  Copyright © 2018 Strahinja Ljaljević. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
